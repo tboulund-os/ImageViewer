b69dcfff6a8c0dd36b59d4f8cd6b68e732056e2b
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-03-22 10:32:09 +0100

    Changed package name

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -1,4 +1,4 @@
-package sample;
+package dk.easv;
 
 import java.io.File;
 import java.net.URL;
--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package dk.easv;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
